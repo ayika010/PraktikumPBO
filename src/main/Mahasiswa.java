c21e794b1a29f9ca80915e9033136dbe31cbd876
ayika010 <starkgement@gmail.com>
2021-03-28 20:47:44 +0700

    TugasPertemuan1

---
+++ src/main/Mahasiswa.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Mahasiswa {
+     String nama,nim;
+     int usia;
+     float uts,uas;
+    public Mahasiswa(String nama,String nim,Integer usia,float uts,float uas){
+        this.nama= nama; 
+        this.nim= nim;
+        this.usia= usia;
+        this.uts= uts;
+        this.uas= uas;
+     }
+     
+     float nilaiAkhir(){
+         return (uts+uas)/2;
+     }
+}
